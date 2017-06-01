4b4eeaa91f7a3273d11557428892159724c77d7a
Achacha35 <achachapeter@gmail.com>
2017-06-01 14:30:42 +0300

    java

---
+++ scripts.js
@@ -0,0 +1,27 @@
+var add = function(number1, number2) {return number1 + number2;};
+var number1 = prompt("Enter number:");
+var number2 = prompt("Enter another number:");
+var number1=parseInt(number1);
+var number2=parseInt(number2);
+alert(add(number1, number2));
+
+var subtract = function(number1, number2) {return number1 - number2;};
+var number1 = prompt("Enter number");
+var number2 = prompt("Enter another number");
+var number1=parseInt(number1);
+var number2=parseInt(number2);
+alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {return number1 * number2;};
+var number1 = prompt("Enter number");
+var number2 = prompt("Enter another number");
+var number1=parseInt(number1);
+var number2=parseInt(number2);
+alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {return number1 / number2;};
+var number1 = prompt("Enter number");
+var number2 = prompt("Enter another number");
+var number1=parseInt(number1);
+var number2=parseInt(number2);
+alert(divide(number1, number2));
